4f0262bee97d362415185822de41d21b57cf593c
emli <emilbeksulaymanov@gmail.com>
2020-03-29 14:54:41 +0600

    SeriesController

---
+++ src/main/java/com/apartment/controller/api/v1/SeriesController.java
@@ -0,0 +1,29 @@
+package com.apartment.controller.api.v1;
+
+import com.apartment.assembler.datatable.SeriesResourceAssembler;
+import com.apartment.resource.datatable.SeriesResource;
+import com.apartment.service.series.SeriesService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/v1/series/")
+public class SeriesController {
+
+    private final SeriesService seriesService;
+    private final SeriesResourceAssembler seriesResourceAssembler;
+
+
+    public SeriesController(SeriesService seriesService, SeriesResourceAssembler seriesResourceAssembler) {
+        this.seriesService = seriesService;
+        this.seriesResourceAssembler = seriesResourceAssembler;
+    }
+
+    @GetMapping("/list")
+    public List<SeriesResource> list(
+            @RequestParam(name = "role", required = false) Long roleId
+    ) {
+        return seriesResourceAssembler.toResources(seriesService.findAll());
+    }
+}
--- src/main/java/com/apartment/service/series/SeriesService.java
+++ src/main/java/com/apartment/service/series/SeriesService.java
@@ -4,6 +4,8 @@ import com.apartment.model.Series;
 import com.apartment.repository.SeriesRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SeriesService {
 
@@ -15,5 +17,9 @@ public class SeriesService {
 
     public Series findById(Long id){
         return repository.findById(id).orElse(null);
+    }
+
+    public List<Series> findAll(){
+        return repository.findAll();
     }
 }
