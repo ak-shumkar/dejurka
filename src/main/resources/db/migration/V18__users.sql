4ac1c004d0eaf19df857cb2deeaafc3ede6648db
emli <emilbeksulaymanov@gmail.com>
2020-06-13 17:27:12 +0600

    add users

--- src/main/resources/db/migration/V18__users.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (created_at, username, password, first_name, last_name, email, enabled)
-values
-    (now(),'rys277@mail.ru', crypt('123456', gen_salt('bf', 8)), '-', '-','rys277@mail.ru',true);
-
