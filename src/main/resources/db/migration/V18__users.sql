b7f14c8139267fa858865dbecbed9f9ef0417a40
emli <emilbeksulaymanov@gmail.com>
2020-06-13 18:00:17 +0600

    add users

---
+++ src/main/resources/db/migration/V18__users.sql
@@ -0,0 +1,3 @@
+alter table house alter column title type character varying(5000);
+alter table house alter column address type character varying(5000);
+alter table house alter column dsc type character varying(5000);
